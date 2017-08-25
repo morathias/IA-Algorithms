f16f17a16452e5fa50a28dde03b8d2ec66eb8dcf
morathias <matias_morales_1996@hotmail.com>
2017-08-24 21:09:52 -0300

    Terminado
    los 4 patrones terminados,
    el editor esta medio roto

---
+++ Assets/Scripts/AIAlgorithms/BreadthFirst.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadthFirst : PathFindingAlgorithm {
+    Queue<Node> _openedNodes;
+
+    public BreadthFirst()
+    {
+        Debug.Log("BreadthFirst");
+        _openedNodes = new Queue<Node>();
+    }
+
+    protected override Node makeCurrentNode()
+    {
+        Node currentNode = _openedNodes.Dequeue();
+
+        return currentNode;
+    }
+
+    protected override void openNode(Node node)
+    {
+        _openedNodes.Enqueue(node);
+    }
+
+    protected override bool isInOpenedNodes(Node node)
+    {
+        return _openedNodes.Contains(node);
+    }
+
+    protected override bool isOpenedNodesEmpty()
+    {
+        if (_openedNodes.Count <= 0)
+            return true;
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMovement : MonoBehaviour {
+    Stack<Vector3> _path;
+    Vector3 _currentPositionToMove;
+    Vector3 _startPosition;
+
+    public float speed;
+    bool _startMoving = false;
+
+    void Start() {
+        _startPosition = transform.position;
+    }
+
+	void Update () {
+        if (_startMoving)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, _currentPositionToMove, speed * Time.deltaTime);
+            transform.position = pos;
+
+            if(pos == _currentPositionToMove)
+            {
+                if (_path.Count > 0)
+                    _currentPositionToMove = _path.Pop();
+            }
+        }
+	}
+
+    public void setPath(Stack<Vector3> path) {
+        _path = path;
+        _currentPositionToMove = _path.Pop();
+        _startMoving = true;
+    }
+    public void reset() {
+        _currentPositionToMove = _startPosition;
+        _startMoving = false;
+        _path.Clear();
+    }
+}
